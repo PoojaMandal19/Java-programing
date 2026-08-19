9ac1952e63899c014ef8d5e8c1d472b481b96495
PoojaMandal19 <poojamandal1905@gmail.com>
2026-08-19 12:41:40 +0530

    Update Java programs

--- basicpractise.java
+++ basicpractise.java
@@ -138,3 +138,22 @@
 // Question5: WriteaJavaprogramthattakesayearfromtheuserandprintwhetherthat
 // year is a leap year or not.
 
+// import java.util.*;
+// public class basicpractise{
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Input the year:");
+//         int year = sc.nextInt();
+
+//         boolean x = (year % 4) == 0;
+//         boolean y = (year % 100) != 0;
+//         boolean z = ((year % 100 == 0) && (year % 400 == 0));
+
+//         if(x && (y || z)){
+//             System.out.println(year + " is a leap year");
+//         } else {
+//             System.out.println(year + " is not a leap year");
+//         }
+//     }
+
+// }
